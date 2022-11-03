037752df2219c834c3aa04a43f7cf31dca4937eb
KnowWisdom <knowisdom314@gmail.com>
2022-11-03 15:16:30 +0900

    prepare to deploy

--- config/settings.py
+++ config/settings.py
@@ -25,10 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-secret_file = os.path.join(BASE_DIR, 'secrets.json')
-
-with open(secret_file) as f:
-    secrets = json.loads(f.read())
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-8)45s^tx7w-2u0e*rm7x1kuk73v#!#1lm1=(n4=(psesh665@l')
 
