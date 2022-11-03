f7ce301d31dd43fbd429d16c65d2eed9c815a1a2
KnowWisdom <knowisdom314@gmail.com>
2022-11-03 16:16:41 +0900

    heroku

--- config/settings.py
+++ config/settings.py
@@ -229,3 +229,7 @@ GRAPH_MODEL = {
     'all_applications' : True,
     'group_models' : True,
 }
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
