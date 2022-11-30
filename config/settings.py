be1fa8f988120a4d2e12c11e9630e76df9ab2aa1
KnowWisdom <knowisdom314@gmail.com>
2022-11-30 15:20:18 +0900

    heroku 새끼..

--- config/settings.py
+++ config/settings.py
@@ -100,12 +100,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangogirls',
-        'USER': 'name',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
@@ -223,7 +219,6 @@ GRAPH_MODEL = {
     'all_applications' : True,
     'group_models' : True,
 }
-
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
