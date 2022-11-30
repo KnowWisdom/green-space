2d87067c37ded11a79b45cbb4e9bb19561f2004a
KnowWisdom <knowisdom314@gmail.com>
2022-11-30 16:51:06 +0900

    deguh 개새..

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '23=ilm*fxfclh8azx(^3qk9vgeos3&
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
